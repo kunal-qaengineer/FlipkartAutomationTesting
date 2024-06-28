d018a568c8423cdf5d79f90a1c0c83359b2e502e
kunal.qaengineer <k.satao@edexa.team>
2024-06-28 18:34:36 +0530

    [add]:- Utility Classes

--- src/main/java/utility/Reports.java
+++ src/main/java/utility/Reports.java
@@ -1,5 +1,22 @@
 package utility;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+
 public class Reports {
+	
+	public static ExtentReports addReports() {
+		
+		ExtentSparkReporter html = new ExtentSparkReporter("extentReport.html");
+		ExtentReports reports = new ExtentReports();
+		reports.attachReporter(html);
+		
+		reports.setSystemInfo("Created By", "Kunal Satao");
+		reports.setSystemInfo("Testing Type", "Functional");
+		reports.setSystemInfo("Project", "Flipkart");
+		reports.setSystemInfo("Modules", "HomePage");
+		
+		return reports;		
+	}
 
 }
--- src/main/java/utility/Screenshot.java
+++ src/main/java/utility/Screenshot.java
@@ -1,5 +1,22 @@
 package utility;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.io.FileHandler;
+
 public class Screenshot {
+	
+	public static void captureScreenshot(WebDriver driver, String name) throws IOException {
+		
+		File source = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+		
+		File destination = new File("C:\\AutomationTesting\\AutomationProjects\\Flipkart\\FlipkartAutomationTesting\\Screenshot\\" + name +".png");
+		
+		FileHandler.copy(source, destination);
+	}
 
 }
